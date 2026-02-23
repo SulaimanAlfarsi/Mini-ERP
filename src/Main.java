80d1c7076ef478e8c3a278b93a46405dfbf50f53
SulaimanAlfarsi <sulaimanalfarsi3060@gmail.com>
2026-02-23 07:19:36 +0400

    Initial CLI ERP project with main menu and navigation loop

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning){
+
+            // Display Main Menu
+            System.out.println("\n=====CLI ERP SYSTEM=====");
+            System.out.println("1. Customer");
+            System.out.println("2. Admin");
+            System.out.println("3. Support Staff");
+            System.out.println("4. Exit");
+
+            //Check if input is a number
+            if (!scanner.hasNext()){
+                System.out.println("Invalid input! Please enter a number between 1-4. ");
+                scanner.next();
+                continue;
+            }
+            int choice = scanner.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Customer module selected.");
+                    break;
+
+                case 2:
+                    System.out.println("Admin module selected.");
+                    break;
+
+                case 3:
+                    System.out.println("Support Staff module selected.");
+                    break;
+
+                case 4:
+                    System.out.println("Exiting system... Goodbye!");
+                    isRunning = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid option! Please choose between 1-4.");
+            }
         }
+        scanner.close();
+
     }
 }
